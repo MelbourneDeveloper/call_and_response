b856fd13ef05c95510e13b033f3c5dd275c16cb1
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-01-23 10:36:09 +1100

    Merge the widget test with the integration test

--- example/integration_test/integration_test.dart
+++ example/integration_test/integration_test.dart
@@ -1,29 +1,5 @@
 
-import 'package:example/fake_server.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
+import '../test/widget_test.dart' as wt;
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    final fakeServerUrl = await fakeServer();
-
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(
-      baseUri: fakeServerUrl,
-    ));
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+void main() => wt.main();
