338e9672f113101823ccb6a85b28974cfb3e1d74
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-01-23 09:53:24 +1100

    Some refactoring

--- example/lib/fake_server.dart
+++ example/lib/fake_server.dart
@@ -9,22 +9,24 @@ class CounterServerState {
       CounterServerState()..count = json["count"] as int;
 }
 
+///This state is private. It means the client app cannot access the state
+///However, there is no reason we can't share state if we need to
+///You can directly debug this code and log any calls to the server
+CounterServerState _counter = CounterServerState();
+
 ///Spin up a HTTP server in the Flutter process that answers calls
 Future<Uri> fakeServer() async {
-  //This state is local to this function. It means
-  CounterServerState counter = CounterServerState();
-
   final server = await (router.Router()
         ..addGet(
           //TODO: Remove nothing
           '/count/<nothing>',
           (r, arg) async {
-            counter.count++;
-            return counter;
+            _counter.count++;
+            return _counter;
           },
           (state) => state.toJson(),
         ))
-      .toServer(8087);
+      .toServer(8088);
 
   return Uri.parse('http://${server.address.host}:${server.port}');
 }
