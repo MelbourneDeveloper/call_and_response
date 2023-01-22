d8496f64add46b122a92ceb33e05ac4c77c06b78
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2023-01-23 10:41:53 +1100

    Notes

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,8 +1,18 @@
 import 'package:example/fake_server.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:example/main.dart';
+
+//This is a Widget test that spins up our fake server and then runs our app
+//that makes actual HTTP calls to the server. This test does not work as a normal
+//widget test because it uses the network. We need to run this as an integration
+//test.
+
+//I was hoping that we could pass an actual HttpClient into the test via HttpOverrides.runZoned
+//But, it seems that if you pass an actual HttpClient in via createHttpClient
+//It will just replace the client with a Mock anyway...
+
+//This is in a sense reasonable because HTTP requires the underlying platform to work
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -19,6 +29,9 @@ void main() {
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
+
+    //This is the only different with the standard Flutter Counter example test
+    //This waits for the HTTP call to execute - i.e. waits for the spinner to stop spinning
     await tester.pumpAndSettle();
 
     // Verify that our counter has incremented.
